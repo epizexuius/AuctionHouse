71cc90317594e8be9726af9a34924e63d378bfec
epizeuxius <markericanthony@gmail.com>
2022-09-05 00:18:38 +0530

    Added createAuction and endAuction and bunch of view functions

--- contracts/AuctionHouse.sol
+++ contracts/AuctionHouse.sol
@@ -2,11 +2,14 @@
 
 pragma solidity >=0.8.0;
 
-contract AuctionHouse {
+import "../interfaces/IERC721.sol";
+import "../interfaces/IERC721Receiver.sol";
+
+contract AuctionHouse is IERC721Receiver {
     struct Auction {
         bool auctionEnded;
         bytes32 auctionTitle;
-        address nftContractAddress;
+        address nft;
         address payable beneficiary;
         address highestBidder;
         uint256 nftTokenId;
@@ -16,11 +19,19 @@ contract AuctionHouse {
     }
 
     uint256 public auctionId = 0;
-    uint256 public commission;
-    address owner;
+    uint256 public commission = 5;
+    address public owner;
 
     mapping(address => uint256) private balances;
     mapping(uint256 => Auction) public auctionIdToAuction;
+
+    event AuctionCreated(
+        uint256 auctionId,
+        bytes32 auctionTitle,
+        address nftContractAddress,
+        uint256 nftId,
+        address beneficiary
+    );
 
     event HighestBidIncreased(
         uint256 auctionId,
@@ -36,9 +47,22 @@ contract AuctionHouse {
         uint256 winningBid
     );
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Not valid address");
+        _;
+    }
+
     function createAuction(
-        uint256 _auctionId,
-        uint256 auctionDuration,
+        uint256 _auctionDuration,
         bytes32 _auctionTitle,
         address _nftContractAddress,
         uint256 _nftTokenId
@@ -51,12 +75,69 @@ contract AuctionHouse {
             msg.sender,
             _nftTokenId,
             block.timestamp,
-            block.timestamp + auctionDuration,
+            block.timestamp + _auctionDuration,
             0
         );
 
-        auctionIdToAuction[auctionId] = auction;
-        auctionId++;
+        //Check if seller owns the NFT
+        require(
+            IERC721(_nftContractAddress).ownerOf(_nftTokenId) == msg.sender,
+            "Seller does not own NFT"
+        );
+        IERC721(_nftContractAddress).safeTransferFrom(
+            msg.sender,
+            address(this),
+            _nftTokenId
+        );
+        //Check if NFT transfer was successful
+        require(
+            IERC721(_nftContractAddress).ownerOf(_nftTokenId) == address(this),
+            "NFT transfer did not go through"
+        );
+
+        auctionIdToAuction[++auctionId] = auction; //Start with auction id 1, presumably ++i is more gas optimal then i++
+        emit AuctionCreated(
+            auctionId,
+            _auctionTitle,
+            _nftContractAddress,
+            _nftTokenId,
+            msg.sender
+        );
+        return true;
+    }
+
+    function bid() public view {}
+
+    function endAuction(uint256 _auctionId) external {
+        require(
+            auctionIdToAuction[_auctionId].auctionEnded == false,
+            "Auction is already closed."
+        );
+        require(
+            auctionIdToAuction[_auctionId].auctionEndTime < block.timestamp,
+            "It is not time yet to close auction"
+        );
+
+        IERC721 token = IERC721(auctionIdToAuction[_auctionId].nft);
+        require(
+            token.ownerOf(auctionIdToAuction[_auctionId].nftTokenId) ==
+                address(this),
+            "Auction does not own this nft"
+        );
+        auctionIdToAuction[_auctionId].auctionEnded = true;
+        balances[
+            auctionIdToAuction[_auctionId].highestBidder
+        ] -= auctionIdToAuction[_auctionId].highestBid;
+        balances[
+            auctionIdToAuction[_auctionId].beneficiary
+        ] += auctionIdToAuction[_auctionId].highestBid;
+
+        //Complete changing all state variables before safeTransfer of nft to guard against reentrancy exploits.
+        token.safeTransferFrom(
+            address(this),
+            auctionIdToAuction[_auctionId].highestBidder,
+            auctionIdToAuction[_auctionId].nftTokenId
+        );
     }
 
     function withdrawBalance() external payable {
@@ -66,15 +147,37 @@ contract AuctionHouse {
         );
         uint256 amount = balances[msg.sender];
         balances[msg.sender] = 0;
-        (bool success, ) = payable(msg.sender).call{value: amount}(""); //Consider using sendValue instead of transfer
+        (bool success, ) = payable(msg.sender).call{value: amount}(""); //Consider using call instead of transfer
         require(
             success,
             "There was some error with the withdraw transaction. It could not be completed."
         );
     }
 
+    function setCommission(uint256 _newCommision) external onlyOwner {
+        commission = (_newCommision > 8) ? 8 : _newCommision;
+    }
+
+    function changeOwner(address _newOwner)
+        public
+        onlyOwner
+        validAddress(_newOwner)
+    {
+        owner = _newOwner;
+    }
+
     function getBalance() external view returns (uint256 balance) {
         return balances[msg.sender];
+    }
+
+    // Read https://eips.ethereum.org/EIPS/eip-721 for more info on this function
+    function onERC721Received(
+        address,
+        address,
+        uint256 _tokenId,
+        bytes memory
+    ) external override returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
     }
 
     receive() external payable {
